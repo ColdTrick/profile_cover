65fe6261acfb1789bfdac214ed7e223120f15691
Jerome Bakker <jeabakker@coldtrick.com>
2018-07-26 11:30:45 +0200

    chore: use new API options

--- classes/ColdTrick/ProfileCover/Menus.php
+++ classes/ColdTrick/ProfileCover/Menus.php
@@ -7,26 +7,27 @@ class Menus {
 	/**
 	 * Add menu items to the user_hover menu
 	 *
-	 * @param string          $hook         the name of the hook
-	 * @param string          $type         the type of the hook
-	 * @param \ElggMenuItem[] $return_value current return value
-	 * @param array           $params       supplied params
+	 * @param \Elgg\Hook $hook 'register', 'menu:user_hover'
 	 *
 	 * @return void|\ElggMenuItem[]
 	 */
-	public static function userHover($hook, $type, $return_value, $params) {
+	public static function userHover(\Elgg\Hook $hook) {
 		
-		$user = elgg_extract('entity', $params);
-		if (!($user instanceof \ElggUser) || !$user->canEdit()) {
+		$user = $hook->getEntityParam()
+		if (!$user instanceof \ElggUser || !$user->canEdit()) {
 			return;
 		}
 		
-		$section = ($user->getGUID() === elgg_get_logged_in_user_guid()) ? 'action' : 'admin';
+		$section = ($user->guid === elgg_get_logged_in_user_guid()) ? 'action' : 'admin';
+		
+		$return_value = $hook->getValue();
 		
 		$return_value[] = \ElggMenuItem::factory([
 			'name' => 'profile_cover',
 			'text' => elgg_echo('profile_cover:menu:edit'),
-			'href' => "cover/edit/{$user->username}",
+			'href' => elgg_generate_url('settings:cover', [
+				'username' => $user->username,
+			]),
 			'section' => $section,
 		]);
 		
@@ -36,28 +37,29 @@ class Menus {
 	/**
 	 * Add menu items to the page menu on the settings page
 	 *
-	 * @param string          $hook         the name of the hook
-	 * @param string          $type         the type of the hook
-	 * @param \ElggMenuItem[] $return_value current return value
-	 * @param array           $params       supplied params
+	 * @param \Elgg\Hook $hook 'register', 'menu:page'
 	 *
 	 * @return void|\ElggMenuItem[]
 	 */
-	public static function settingsPage($hook, $type, $return_value, $params) {
+	public static function settingsPage(\Elgg\Hook $hook) {
 		
 		if (!elgg_in_context('settings')) {
 			return;
 		}
 		
 		$page_owner = elgg_get_page_owner_entity();
-		if (!($page_owner instanceof \ElggUser) || !$page_owner->canEdit()) {
+		if (!$page_owner instanceof \ElggUser || !$page_owner->canEdit()) {
 			return;
 		}
+		
+		$return_value = $hook->getValue();
 		
 		$return_value[] = \ElggMenuItem::factory([
 			'name' => 'profile_cover',
 			'text' => elgg_echo('profile_cover:menu:edit'),
-			'href' => "cover/edit/{$page_owner->username}",
+			'href' => elgg_generate_url('settings:cover', [
+				'username' => $user->username,
+			]),
 			'section' => '1_profile',
 		]);
 		
