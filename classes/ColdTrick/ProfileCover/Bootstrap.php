a1ba52860b07f1ba78cf54a2745f69646b0f9e8c
Jerome Bakker <jeabakker@coldtrick.com>
2018-07-26 11:07:00 +0200

    changed: moved contents of start.php to Bootstrap and elgg-plugin.php

---
+++ classes/ColdTrick/ProfileCover/Bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ColdTrick\ProfileCover;
+
+use Elgg\DefaultPluginBootstrap;
+
+class Bootstrap extends DefaultPluginBootstrap {
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function init() {
+		
+		// css/js
+		elgg_extend_view('css/elgg', 'css/cover/site.css');
+		
+		// plugin hooks
+		$hooks = $this->elgg()->hooks;
+		$hooks->registerHandler('register', 'menu:user_hover', __NAMESPACE__ . '\Menus::userHover');
+		$hooks->registerHandler('register', 'menu:page', __NAMESPACE__ . '\Menus::settingsPage');
+		
+		$hooks->registerHandler('entity:profile_cover:sizes', 'user', __NAMESPACE__ . '\CoverIcon::sizes');
+		$hooks->registerHandler('entity:profile_cover:saved', 'user', __NAMESPACE__ . '\CoverIcon::saved');
+		$hooks->registerHandler('entity:profile_cover:delete', 'user', __NAMESPACE__ . '\CoverIcon::delete');
+	}
+}
---
+++ elgg-plugin.php
@@ -0,0 +1,22 @@
+<?php
+
+use ColdTrick\ProfileCover\Bootstrap;
+use Elgg\Router\Middleware\Gatekeeper;
+
+return [
+	'bootstrap' => Bootstrap::class,
+	'actions' => [
+		'cover/upload' => [],
+		'cover/remove' => [],
+		'cover/crop' => [],
+	],
+	'routes' => [
+		'settings:cover' => [
+			'path' => 'settings/cover/{username}',
+			'middleware' => [
+				Gatekeeper::class,
+			],
+			'resource' => 'settings/cover',
+		],
+	],
+];
