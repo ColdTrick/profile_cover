f76793a7b2b4c53c193dbcb78de657ca9a10eb6a
Jerome Bakker <jeabakker@coldtrick.com>
2018-12-14 16:41:15 +0100

    added: Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file was created by Translation Editor v7.0-beta
+ * On 2018-12-14 16:40
+ */
+
+return array (
+  'profile_cover:menu:edit' => 'Bewerk profiel achtergrond',
+  'profile_cover:upgrade:2018121400:title' => 'Migreer profiel achtergrond uitsnede coördinaten',
+  'profile_cover:upgrade:2018121400:description' => 'Verplaats de opgeslagen profiel achtergrond uitsnede coördinaten naar de standaard Elgg locatie.',
+  'profile_cover:settings:height' => 'Profiel achtergrond hoogte (in px)',
+  'profile_cover:settings:width' => 'Profiel achtergrond breedte (in px)',
+  'profile_cover:edit:title' => 'Bewerk profiel achtergrond',
+  'profile_cover:edit:description' => 'Hier kun je een afbeelding uploaden welke gebruikt zal worden als je profiel achtergrond.',
+  'profile_cover:upload:cover_image:help' => 'De optimale afmetingen voor de achtergrond afbeelding zijn %dx%dpx.',
+  'profile_cover:action:upload:success' => 'Je profiel achtergrond afbeelding is opgeslagen',
+  'profile_cover:action:upload:error' => 'Er is een fout opgetreden tijdens het opslaan van je profiel achtergrond afbeelding',
+  'profile_cover:action:remove:success' => 'Je profiel achtergrond afbeelding is verwijderd',
+);
