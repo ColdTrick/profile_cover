49fe305a7c18cdf1847cbe3352ee8ab59a677c28
Jeroen Dalsem <jdalsem@coldtrick.com>
2022-07-01 11:50:50 +0200

    chore: updated dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -12,7 +12,7 @@ return array (
   'profile_cover:settings:height' => 'Profiel achtergrond hoogte (in px)',
   'profile_cover:settings:width' => 'Profiel achtergrond breedte (in px)',
   'profile_cover:edit:title' => 'Bewerk profiel achtergrond',
-  'profile_cover:edit:description' => 'Hier kun je een afbeelding uploaden welke gebruikt zal worden als je profiel achtergrond.',
+  'profile_cover:edit:description' => 'Hier kun je een afbeelding uploaden die getoond wordt als profiel achtergrond.',
   'profile_cover:upload:cover_image:help' => 'De optimale afmetingen voor de achtergrond afbeelding zijn %dx%dpx.',
   'profile_cover:action:upload:success' => 'Je profiel achtergrond afbeelding is opgeslagen',
   'profile_cover:action:upload:error' => 'Er is een fout opgetreden tijdens het opslaan van je profiel achtergrond afbeelding',
